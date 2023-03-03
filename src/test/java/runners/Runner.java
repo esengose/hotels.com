4e43e6ba75368b3a85bf2fe0996448668ccda0f3
esengose <eseng1801@gmail.com>
2023-03-03 13:25:06 +0300

    3.3

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,23 @@
+package runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+   features="src/test/resources/features",
+   glue="stepdefinitions",
+   tags="",
+   dryRun=false
+
+
+
+)
+
+
+
+public class Runner {
+
+}
