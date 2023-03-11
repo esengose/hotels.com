e53b3a2b6e4b9a56a0691d668dc7cefca1b1fc47
Zeynep Cuvalci <cvlcizeynep@outlook.com>
2023-03-11 20:42:12 +0300

    11//03

--- src/test/java/runners/R_Runner.java
+++ src/test/java/runners/R_Runner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
         features="src/test/resources/features",
         glue="stepdefinitions",
         tags="@hotelscom_login",
-        dryRun=true
+        dryRun=false
+
 
 )
 
